115f57a69fc7d697a709eb780cd7e245d4495012
Muhammad Awwab Tahir <awwabtahir11@gmail.com>
2020-03-10 14:17:16 +0500

    places app created

--- src/index.js
+++ src/index.js
@@ -4,7 +4,16 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const placeList = [
+    {id: "1", name: "Rawalpindi", state: "Punjab"}, 
+    {id: "2", name: "Islamabad", state: "Federal"}, 
+    {id: "3", name: "Karachi", state: "Sindh"},
+];
+
+ReactDOM.render(
+    <App places={placeList} />,
+    document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
